6434cb3d2c7a0abd9b60c4580bf97b513e78e6b4
Robin Long <r.long@lancaster.ac.uk>
2017-06-08 17:00:50 +0100

    Modified class_input.R to include fpop.

--- R/class_input.R
+++ R/class_input.R
@@ -6,11 +6,14 @@ class_input <- function(data, cpttype, method, test.stat, penalty, pen.value, mi
     ans=new("cpt")
   }
   data.set(ans)=data;cpttype(ans)=paste(cpttype," (",test.stat,")", sep = "");method(ans)=method; test.stat(ans)=test.stat;pen.type(ans)=penalty;pen.value(ans)=pen.value;minseglen(ans)=minseglen;
-  if(penalty!="CROPS"){ # crops is only one that doesn't give a single set of cpts
+  if(method=="FPOP"){
+    cpts(ans)=out$cpts
+    param.est(ans)=list(mean=out$mean.path)
+  }
+  else if(penalty!="CROPS"){ # crops is only one that doesn't give a single set of cpts
     cpts(ans)=out[[2]]
   }
-
-  if(penalty=="CROPS"){
+  else{#(penalty=="CROPS")
     m = t(sapply(out[[2]], '[', 1:max(sapply(out[[2]], length))))
 
     cpts.full(ans) = m
